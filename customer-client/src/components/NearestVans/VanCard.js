b9272b2b2690433a976c846c28340d85332fa820
Ben Cook <mcbeanoo@gmail.com>
2021-03-17 21:44:54 +1100

    improved formatting

--- customer-client/src/components/NearestVans/VanCard.js
+++ customer-client/src/components/NearestVans/VanCard.js
@@ -1,24 +1,27 @@
 import * as React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import { Card, CardContent, CardMedia, Typography } from '@material-ui/core'
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+  Grid,
+} from '@material-ui/core'
 import Ratings from 'react-ratings-declarative'
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexDirection: 'row',
-    flexWrap: 'nowrap',
-    justifyContent: 'space-between',
-    height: '100px',
-  },
+  root: {},
   media: {
-    height: '100%',
-    width: '150px',
+    // height: '100%',
+    // width: '100%',
   },
   content: {
-    display: 'flex',
-    flexDirection: 'column',
-    margin: 'auto',
+    // display: 'flex',
+    // flexDirection: 'column',
+    // margin: 'auto',
+  },
+  distanceText: {
+    float: 'right',
   },
 }))
 
@@ -27,32 +30,61 @@ const VanCard = (props) => {
 
   return (
     <Card className={styles.root}>
-      <CardMedia
-        component="img"
-        className={styles.media}
-        image={'foodTrucks/foodTruck.jpg'}
-        title="Food Truck"
-      ></CardMedia>
+      <Grid
+        container
+        direction="row"
+        justify="space-between"
+        alignItems="center"
+        spacing={0}
+      >
+        <Grid item xs={2}>
+          <CardMedia
+            component="img"
+            className={styles.media}
+            image={'foodTrucks/foodTruck.jpg'}
+            title="Food Truck"
+          ></CardMedia>
+        </Grid>
 
-      <CardContent className={styles.content}>
-        <Typography variant="h5" component="h2" className={styles.content}>
-          {props.title}
-        </Typography>
-        <Typography variant="body1" component="p">
-          {props.simpleLocation}
-        </Typography>
-        <Ratings
-          rating={parseFloat(props.rating)}
-          widgetDimensions="30px"
-          widgetSpacings="8px"
-        >
-          <Ratings.Widget widgetRatedColor="orange"></Ratings.Widget>
-          <Ratings.Widget widgetRatedColor="orange"></Ratings.Widget>
-          <Ratings.Widget widgetRatedColor="orange"></Ratings.Widget>
-          <Ratings.Widget widgetRatedColor="orange"></Ratings.Widget>
-          <Ratings.Widget widgetRatedColor="orange"></Ratings.Widget>
-        </Ratings>
-      </CardContent>
+        <Grid item xs={4}>
+          <CardContent className={styles.content}>
+            <Typography variant="h5" component="h2" className={styles.content}>
+              {props.title}
+            </Typography>
+            <Typography variant="body1" component="p">
+              {props.simpleLocation}
+            </Typography>
+          </CardContent>
+        </Grid>
+
+        <Grid item xs={4}>
+          <CardContent className={styles.content}>
+            <Ratings
+              rating={parseFloat(props.rating)}
+              widgetDimensions="30px"
+              widgetSpacings="8px"
+            >
+              <Ratings.Widget widgetRatedColor="orange"></Ratings.Widget>
+              <Ratings.Widget widgetRatedColor="orange"></Ratings.Widget>
+              <Ratings.Widget widgetRatedColor="orange"></Ratings.Widget>
+              <Ratings.Widget widgetRatedColor="orange"></Ratings.Widget>
+              <Ratings.Widget widgetRatedColor="orange"></Ratings.Widget>
+            </Ratings>
+          </CardContent>
+        </Grid>
+
+        <Grid item xs={2}>
+          <CardContent>
+            <Typography
+              variant="body1"
+              component="p"
+              className={styles.distanceText}
+            >
+              Distance: {props.distance}km
+            </Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
     </Card>
   )
 }
