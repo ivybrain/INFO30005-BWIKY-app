d84927e9e0722df2cdcfb148b1a5b383d5f0a424
Ben Cook <mcbeanoo@gmail.com>
2021-03-17 13:51:51 +1100

    add favicon

---
+++ customer-client/src/components/Nav.js
@@ -0,0 +1,12 @@
+import { AppBar, Toolbar, IconButton, Typography, Button, Drawer, List } from "@material-ui/core";
+
+const Nav = (props) => {
+
+    return (
+        <>
+            <AppBar></AppBar>
+        </>
+    )
+}
+
+export default Nav;
