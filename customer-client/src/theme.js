64998495616dc71b833f13d3735c32fc69945974
Ben Cook <mcbeanoo@gmail.com>
2021-03-17 18:14:15 +1100

    create theme

---
+++ customer-client/src/theme.js
@@ -0,0 +1,14 @@
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      //   main: "",
+    },
+    background: {
+      default: '#ffffff',
+    },
+  },
+})
+
+export default theme
