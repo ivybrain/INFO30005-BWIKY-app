cfd597ecf731e6cfab4493128a4187fcdf5b126c
Ben Cook <mcbeanoo@gmail.com>
2021-03-18 01:35:21 +1100

    changed component structure

--- customer-client/src/components/Nav2.js
+++ customer-client/src/components/Nav2.js
@@ -9,6 +9,7 @@ import Box from '@material-ui/core/Box'
 import { Toolbar, IconButton, Grid } from '@material-ui/core'
 import Logo from './Logo/Logo'
 import theme from '../theme'
+import PageHandler from './PageHandler'
 
 function a11yProps(index) {
   return {
@@ -19,11 +20,11 @@ function a11yProps(index) {
 
 const useNavStyles = makeStyles((theme) => ({
   root: {
-    // backgroundColor: theme.palette.background.paper,
+    backgroundColor: theme.palette.background.default,
     width: '100%',
   },
   appBar: {
-    // background: theme.white,
+    // background: 'in',
     boxShadow: '0px 0px 0px 0px',
   },
   tabs: {
@@ -33,7 +34,7 @@ const useNavStyles = makeStyles((theme) => ({
   },
 }))
 
-export default function Nav2() {
+const Nav2 = () => {
   const classes = useNavStyles()
   //   const theme = useTheme()
   const [value, setValue] = React.useState(0)
@@ -43,47 +44,52 @@ export default function Nav2() {
   }
 
   return (
-    <div className={classes.root}>
-      <AppBar
-        position="static"
-        color="default"
-        style={{ boxShadow: '0px 0px 0px 0px' }}
-      >
-        <Grid
-          container
-          direction="row"
-          justify="space-between"
-          alignItems="center"
+    <>
+      <div className={classes.root}>
+        <AppBar
+          position="static"
+          color="default"
+          style={{ boxShadow: '0px 0px 0px 0px', backgroundColor: '#fafafa' }}
         >
-          <Grid item xs={1}></Grid>
-          <Grid item xs={7}>
-            <Toolbar>
-              <IconButton edge="start" aria-label="menu">
-                <Logo></Logo>
-              </IconButton>
-              <Typography variant="h6">SNACKS IN A VAN</Typography>
-            </Toolbar>
+          <Grid
+            container
+            direction="row"
+            justify="space-between"
+            alignItems="center"
+          >
+            <Grid item xs={1}></Grid>
+            <Grid item xs={7}>
+              <Toolbar>
+                <IconButton edge="start" aria-label="menu">
+                  <Logo></Logo>
+                </IconButton>
+                <Typography variant="h6">SNACKS IN A VAN</Typography>
+              </Toolbar>
+            </Grid>
+            <Grid item xs={4} style={{ height: '100%' }}>
+              {/* <div style={{}}>Snacks</div> */}
+              <Tabs
+                value={value}
+                onChange={handleChange}
+                indicatorColor="primary"
+                textColor="primary"
+                // variant="fullWidth"
+                aria-label="navigation tabs"
+                //   flexContainer
+                className={classes.tabs}
+                style={{ height: '100%' }}
+              >
+                <Tab label="Find a van" {...a11yProps(0)} />
+                <Tab label="Log in / Sign Up" {...a11yProps(1)} />
+                <Tab label="My Order" {...a11yProps(2)} />
+              </Tabs>
+            </Grid>
           </Grid>
-          <Grid item xs={4} style={{ height: '100%' }}>
-            {/* <div style={{}}>Snacks</div> */}
-            <Tabs
-              value={value}
-              onChange={handleChange}
-              indicatorColor="primary"
-              textColor="primary"
-              // variant="fullWidth"
-              aria-label="navigation tabs"
-              //   flexContainer
-              className={classes.tabs}
-              style={{ height: '100%' }}
-            >
-              <Tab label="Find a van" {...a11yProps(0)} />
-              <Tab label="Log in / Sign Up" {...a11yProps(1)} />
-              <Tab label="My Order" {...a11yProps(2)} />
-            </Tabs>
-          </Grid>
-        </Grid>
-      </AppBar>
-    </div>
+        </AppBar>
+      </div>
+      <PageHandler page={value}></PageHandler>
+    </>
   )
 }
+
+export default Nav2
---
+++ customer-client/src/components/Pages/SplashPage.js
@@ -0,0 +1,12 @@
+import { Container } from '@material-ui/core'
+import NearestVans from '../NearestVans/NearestVans'
+
+const SplashPage = (props) => {
+  return (
+    <Container>
+      <NearestVans></NearestVans>
+    </Container>
+  )
+}
+
+export default SplashPage
