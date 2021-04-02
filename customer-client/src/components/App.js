a5859bf78f868f3a60c14244a11da6188b52eb55
Ben Cook <mcbeanoo@gmail.com>
2021-04-02 16:13:18 +1100

    remove unused imports

--- customer-client/src/components/App.js
+++ customer-client/src/components/App.js
@@ -1,9 +1,7 @@
 import * as React from 'react'
-import { Container, CssBaseline } from '@material-ui/core'
+import { CssBaseline } from '@material-ui/core'
 // import Nav from './Nav'
 import Nav2 from './Nav2'
-import NearestVans from './NearestVans/NearestVans'
-import PageHandler from './PageHandler'
 
 const App = () => {
   return (
