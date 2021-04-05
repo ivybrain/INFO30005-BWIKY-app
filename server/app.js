8111e220eeda4494950aee2eec9dc55b9f7ef6a1
Ivy Brain <ibrain@student.unimelb.edu.au>
2021-04-06 09:54:47 +1000

    Establishes database connection

--- server/app.js
+++ server/app.js
@@ -1,7 +1,12 @@
 const express = require('express');
-const app = express()
-const port = 8080
+const mongoose = require('mongoose');
 
+const dotenv = require('dotenv');
+const dotenv_expand = require('dotenv-expand');
+var env = dotenv.config()
+dotenv_expand(env)
+
+const app = express()
 
 const vendorsR = require('./routes/vendorsR.js');
 const itemsR = require('./routes/itemsR.js');
@@ -12,6 +17,12 @@ app.use(express.urlencoded({ extended: true })) // for parsing application/x-www
 app.use('/vendors', vendorsR);
 app.use('/items', itemsR);
 
-app.listen(port, function () {
-  console.log(`Ready, server running on ${port}`)
+app.listen(function () {
+  mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  });
+  console.log(`Ready, server running on ${process.env.PORT}`)
 })
