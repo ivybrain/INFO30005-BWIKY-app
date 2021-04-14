98695db10eea27f6c9149ade94a2995bc74664d1
Ivy Brain <ibrain@student.unimelb.edu.au>
2021-04-14 13:25:38 +1000

    tests new origin

--- server/controllers/vendorsC.js
+++ server/controllers/vendorsC.js
@@ -5,25 +5,39 @@ const mongoose = require('mongoose');
 const Vendor = mongoose.model('Vendor')
 
 exports.vendor_list = function(req, res) {
+
   var vendors = Vendor.find({}, function(err, docs) {
-    res.send(docs);
+    res.json(docs);
   })
 };
 
 exports.vendor_details = function(req, res) {
   var vendor = Vendor.findById(req.params["vendor_id"], function(err, docs) {
-    res.send(docs);
+    res.json(docs);
   })
 
 };
 
 exports.vendor_create = function(req, res) {
-  filter_incoming(req, res);
-  var new_vendor = new Vendor(req.body["vendor"]);
 
-  new_vendor.save();
+  outputs = []
+  for (i=0; i < req.body.length; i++) {
+    const new_vendor = new Vendor(req.body[i]);
+
+    if (new_vendor.validate()) {
+
+      outputs.push(new_vendor);
+    }
+
+  }
+
+  for (i=0; i<outputs.length; i++) {
+    outputs[i].save()
+  }
+
   res.status(201);
-  res.json(new_vendor);
+  res.json(outputs);
+
 
 };
 
@@ -35,10 +49,4 @@ exports.vendor_delete = function(req, res) {
   Vendor.findByIdAndDelete(req.params["vendor_id"], function(err, docs) {
     res.send(docs);
   });
-}
-
-function filter_incoming(req, res) {
-  if (!req.body.hasOwnProperty('vendor')) {
-    res.sendStatus(400);
-  }
 }
