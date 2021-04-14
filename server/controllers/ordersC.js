6beb8ec7cc729689696652955f44842097880b0a
Ben Cook <mcbeanoo@gmail.com>
2021-04-15 01:04:07 +1000

    add async await to controllers

--- server/controllers/ordersC.js
+++ server/controllers/ordersC.js
@@ -1,41 +1,38 @@
-require('../models/Vendor');
-require('../models/Order');
-const mongoose = require('mongoose');
-
-const Vendor = mongoose.model('Vendor');
-const Order = mongoose.model('Order');
-
-
 // GET /vendors/:vendor_id/orders/
-exports.order_list = function(req, res) {
-  res.send("List of orders for the current vendor");
+exports.order_list = function (req, res) {
+  res.send('List of orders for the current vendor')
 }
 
 // GET /vendors/:vendor_id/orders/:order_id
-exports.order_details = function(req, res) {
-  var order = Vendor.findById(req.params["order_id"], function(err, docs) {})
-  if (order.vendor != req.params["vendor_id"]) {
-    res.sendStatus(404);
-    return;
-  }
-
-  res.json(order)
-
-
+exports.order_details = function (req, res) {
+  res.send('Gets specific order')
 }
 
 // PATCH /vendors/:vendor_id/orders/:order_id
-exports.order_update = function(req, res) {
-  res.send("Updates an order");
+exports.order_update = async (req, res) => {
+  // const vendor_id = req.params['vendor_id']
+  // const order_id = req.params['order_id']
+  // try {
+  //   const updatedVendor = await Vendor.findOneAndUpdate(
+  //     query,
+  //     { $set: req.body },
+  //     { new: true },
+  //   )
+  //   res.status(200)
+  //   res.json(updatedVendor)
+  // } catch (err) {
+  //   res.status(500)
+  // }
+  res.send('PATCH not implemented')
 }
 
 // DELETE /vendors/:vendor_id/orders/:order_id
 // NOTE: Implement as soft delete
-exports.order_delete = function(req, res) {
-  res.send("Deletes an order");
+exports.order_delete = function (req, res) {
+  res.send('Deletes an order')
 }
 
 // POST /vendors/:vendor_id/orders/
-exports.order_create = function(req, res) {
-  res.send("Creates one or more orders");
+exports.order_create = function (req, res) {
+  res.send('Creates one or more orders')
 }
