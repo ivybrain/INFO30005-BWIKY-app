0d52729d3eea6fe8bc4ddaa13e41564c15537cda
Ben Cook <mcbeanoo@gmail.com>
2021-04-16 16:46:28 +1000

    change order path

--- vendor-client/src/App.js
+++ vendor-client/src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import LoginScreen from "./components/LoginScreen";
 import Checkin from "./components/Checkin/Checkin";
 import Order from "./components/Order/Order";
+import Orders from "./components/Orders/Orders";
 
 function App() {
   return (
@@ -10,8 +11,9 @@ function App() {
       <CssBaseline />
       <Switch>
         <Route exact path="/checkin" component={Checkin} />
+        <Route exact path="/orders" component={Orders} />
         <Route
-          path="/order/:id"
+          path="/orders/:id"
           render={(props) => <Order {...props}></Order>}
         />
         <Route exact path="/" component={LoginScreen} />
