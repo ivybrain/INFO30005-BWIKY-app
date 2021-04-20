5d26649359b1f8ef52acdf66ef7385c328eba868
Ben Cook <mcbeanoo@gmail.com>
2021-04-20 12:50:14 +1000

    add GET fulfilled orders

--- server/routes/ordersR.js
+++ server/routes/ordersR.js
@@ -12,14 +12,12 @@ router
   .route('/vendors/:vendor_id([0-9a-fA-F]+)/orders')
   .get(ordersC.order_list)
   .post(ordersC.order_create)
+  .delete(ordersC.order_delete_all)
 
 router
-  .route('/vendors/:vendor_id([0-9a-fA-F]+)/orders/?fulfilled=false')
-  .get(ordersC.orders_unfulfilled)
-router
-   .route('/vendors/:vendor_id([0-9a-fA-F]+)/orders/:order_id([0-9a-fA-F]+)')
-   .get(ordersC.order_details)
+  .route('/vendors/:vendor_id([0-9a-fA-F]+)/orders/:order_id([0-9a-fA-F]+)')
+  .get(ordersC.order_details)
 //   .patch(ordersC.order_update)
-//   .delete(ordersC.order_delete)
+// .delete(ordersC.order_delete)
 
 module.exports = router
