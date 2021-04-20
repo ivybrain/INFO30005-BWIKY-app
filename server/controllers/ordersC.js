44715135ebae5382d120c4b88b59605def2bd2d0
Ivy Brain <ibrain@student.unimelb.edu.au>
2021-04-20 11:16:16 +1000

    Removes package lock from repo

--- server/controllers/ordersC.js
+++ server/controllers/ordersC.js
@@ -7,17 +7,13 @@ const Order = mongoose.model('Order');
 
 
 // GET /vendors/:vendor_id/orders/
-exports.order_list = function(req, res) {
+exports.order_list = async(req, res) => {
   res.send("List of orders for the current vendor");
 }
 
 // GET /vendors/:vendor_id/orders/:order_id
-exports.order_details = function(req, res) {
-  var order = Vendor.findById(req.params["order_id"], function(err, docs) {})
-  if (order.vendor != req.params["vendor_id"]) {
-    res.sendStatus(404);
-    return;
-  }
+exports.order_details = async(req, res) => {
+  var order = await Vendor.findById(req.params["order_id"]);
 
   res.json(order)
 
@@ -25,17 +21,17 @@ exports.order_details = function(req, res) {
 }
 
 // PATCH /vendors/:vendor_id/orders/:order_id
-exports.order_update = function(req, res) {
+exports.order_update = async(req, res) => {
   res.send("Updates an order");
 }
 
 // DELETE /vendors/:vendor_id/orders/:order_id
 // NOTE: Implement as soft delete
-exports.order_delete = function(req, res) {
+exports.order_delete = async(req, res) => {
   res.send("Deletes an order");
 }
 
 // POST /vendors/:vendor_id/orders/
-exports.order_create = function(req, res) {
+exports.order_create = async(req, res) => {
   res.send("Creates one or more orders");
 }
