812f95fbc2999156ea031f1743609fea8e1bf815
Ivy Brain <ibrain@student.unimelb.edu.au>
2021-04-20 12:07:14 +1000

    heroku works

--- server/app.js
+++ server/app.js
@@ -18,8 +18,6 @@ app.use('/vendors', vendorsR)
 app.use('/items', itemsR)
 
 app.listen(process.env.PORT, function () {
-  console.log(process.env.PORT);
-  console.log(process.env.DB_URL)
   mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
