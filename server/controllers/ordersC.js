0b8f98faad32954f4dc2b7c22e7fb6da33c4836e
Wen Yee Ang <wenyee-ang@users.noreply.github.com>
2021-04-21 23:34:11 +1000

    Added missing order_create function.

--- server/controllers/ordersC.js
+++ server/controllers/ordersC.js
@@ -32,11 +32,45 @@ exports.order_list = async (req, res) => {
 
 // GET /vendors/:vendor_id/orders/:order_id
 exports.order_details = async(req, res) => {
-  var order = await Vendor.findById(req.params["order_id"]);
+  //var order = await Vendor.findById(req.params["order_id"]);
+  try {
+    const orders = await Order.find({ vendor: req.params['vendor_id'] })
+
+    var found = orders.find(function (order, index) {
+      if (order._id == req.params['order_id']) return true
+    })
+
+    if (found == null) {
+      res.send('Order not found.')
+    } else {
+      res.status(201)
+      res.json(found)
+    }
+  } catch (err) {
+    return res.status(409).send(err)
+  }
 
   res.json(order)
 
 
+}
+
+// POST /vendors/:vendor_id/orders/
+exports.order_create = async (req, res) => {
+  try {
+    const outputs = await Order.create(req.body)
+    res.status(201)
+    res.json(outputs)
+  } catch (err) {
+    return res.status(409).send(err)
+  }
+}
+
+// DELETE /vendors/:vendor_id/orders
+// NOTE: Implement as soft delete
+exports.order_delete_all = async (req, res) => {
+  await Order.deleteMany({ vendor: req.params.vendor_id })
+  res.status(200).send()
 }
 
 // PATCH /vendors/:vendor_id/orders/:order_id
@@ -48,9 +82,4 @@ exports.order_update = async(req, res) => {
 // NOTE: Implement as soft delete
 exports.order_delete = async(req, res) => {
   res.send("Deletes an order");
-}
-
-// POST /vendors/:vendor_id/orders/
-exports.order_create = async(req, res) => {
-  res.send("Creates one or more orders");
 }
