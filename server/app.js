e8abead588cc6c9a341609d9568b800825a9f8c5
Ben Cook <bpcook@student.unimelb.edu.au>
2021-04-29 18:23:33 +1000

    add cors support (#13)

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const dotenv = require('dotenv')
 const dotenv_expand = require('dotenv-expand')
@@ -7,6 +8,7 @@ var env = dotenv.config()
 dotenv_expand(env)
 
 const app = express()
+app.use(cors())
 
 const vendorsR = require('./routes/vendorsR.js')
 const itemsR = require('./routes/itemsR.js')
@@ -19,15 +21,17 @@ app.use('/items', itemsR)
 app.use(express.static('public'))
 
 app.listen(process.env.PORT, function () {
-  mongoose.connect(process.env.DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  }, function (err) {
-    console.log(err);
-    console.log(`Ready, server running on ${process.env.PORT}`);
-
-  })
-
+  mongoose.connect(
+    process.env.DB_URL,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    },
+    function (err) {
+      console.log(err)
+      console.log(`Ready, server running on ${process.env.PORT}`)
+    },
+  )
 })
