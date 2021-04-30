57cb6d8aabbba237588fed8c7190c855301e0127
Ben Cook <mcbeanoo@gmail.com>
2021-04-30 13:47:33 +1000

    add cancel and confirm order buttons

--- customer-client/src/components/Pages/MyOrder.js
+++ customer-client/src/components/Pages/MyOrder.js
@@ -7,65 +7,111 @@ import {
   TableContainer,
   TableHead,
   TableRow,
+  Button,
+  Grid,
 } from '@material-ui/core'
+import axios from 'axios'
+import { API_URL } from '../'
 
 const columns = ['Item', 'Qty', 'Subtotal']
 
-var audFormatter = new Intl.NumberFormat('en-AU', {
+const audFormatter = new Intl.NumberFormat('en-AU', {
   style: 'currency',
   currency: 'AUD',
 })
 
 const MyOrder = (props) => {
-  const { order } = props
+  const { order, setOrder } = props
+
+  const handleCancelOrder = (e) => {
+    e.preventDefault()
+    setOrder({ items: {} })
+    console.log('cleared order')
+  }
+
+  const handleConfirmOrder = (e) => {
+    e.preventDefault()
+    const headers = { 'Access-Control-Allow-Origin': '*' }
+    // Requires a customer ID, so implement after authentication
+
+    // axios
+    //   .post(`${API_URL}/vendors/:vendor_id/orders`, {
+    //     headers,
+    //   })
+    //   .then((res) => {
+    //     console.log(res)
+    //   })
+    //   .catch((err) => {
+    //     console.error(err)
+    //   })
+  }
 
   return (
     <Container>
       <Typography variant="h2">My Order</Typography>
       {order.items && Object.keys(order.items).length !== 0 ? (
-        <TableContainer>
-          <Table>
-            <TableHead>
-              <TableRow>
-                {columns.map((column) => (
-                  <TableCell key={column}>{column}</TableCell>
-                ))}
-              </TableRow>
-            </TableHead>
+        <>
+          <TableContainer>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  {columns.map((column) => (
+                    <TableCell key={column}>{column}</TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
 
-            <TableBody>
-              {Object.keys(order.items).map((id, idx) => (
-                <TableRow key={idx}>
-                  <TableCell>{order.items[id].item_name}</TableCell>
-                  <TableCell>{order.items[id].quantity}</TableCell>
+              <TableBody>
+                {Object.keys(order.items).map((id, idx) => (
+                  <TableRow key={idx}>
+                    <TableCell>{order.items[id].item_name}</TableCell>
+                    <TableCell>{order.items[id].quantity}</TableCell>
+                    <TableCell>
+                      {audFormatter.format(
+                        order.items[id].quantity * order.items[id].item_price,
+                      )}
+                    </TableCell>
+                  </TableRow>
+                ))}
+                <TableRow>
+                  <TableCell></TableCell>
+                  <TableCell></TableCell>
                   <TableCell>
-                    {audFormatter.format(
-                      order.items[id].quantity * order.items[id].item_price,
-                    )}
+                    <Typography variant="subtitle2" gutterBottom>
+                      Total:{' '}
+                      {audFormatter.format(
+                        Object.keys(order.items)
+                          .map(
+                            (id) =>
+                              order.items[id].quantity *
+                              order.items[id].item_price,
+                          )
+                          .reduce((a, b) => a + b),
+                      )}
+                    </Typography>
                   </TableCell>
                 </TableRow>
-              ))}
-              <TableRow>
-                <TableCell></TableCell>
-                <TableCell></TableCell>
-                <TableCell>
-                  <Typography variant="subtitle2" gutterBottom>
-                    Total:{' '}
-                    {audFormatter.format(
-                      Object.keys(order.items)
-                        .map(
-                          (id) =>
-                            order.items[id].quantity *
-                            order.items[id].item_price,
-                        )
-                        .reduce((a, b) => a + b),
-                    )}
-                  </Typography>
-                </TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
-        </TableContainer>
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <br />
+          <Grid container style={{ justifyContent: 'space-around' }}>
+            <Grid item>
+              <Button variant="outlined" onClick={handleCancelOrder}>
+                <Typography variant="button" display="block" gutterBottom>
+                  Cancel Order
+                </Typography>
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button variant="outlined" onClick={handleConfirmOrder}>
+                <Typography variant="button" display="block" gutterBottom>
+                  Confirm Order
+                </Typography>
+              </Button>
+            </Grid>
+          </Grid>
+        </>
       ) : (
         <Typography variant="subtitle">
           Your order is empty! Try adding some items to your order.
