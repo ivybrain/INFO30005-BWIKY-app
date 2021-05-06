1743962c082d2d4b758b2a3ce69f40b1ab4b8e82
Ben Cook <mcbeanoo@gmail.com>
2021-05-06 14:27:34 +1000

    add login page for when already logged in

--- client/src/customer/Pages/Login.js
+++ client/src/customer/Pages/Login.js
@@ -8,13 +8,19 @@ import {
 
 import axios from 'axios'
 import jwt from 'jsonwebtoken'
+import { useHistory } from 'react-router'
 import { API_URL } from '../../constants'
 
 const Login = (props) => {
-  const { setAuth } = props
+  const { auth, setAuth } = props
+
+  const history = useHistory()
 
   const handle_form_submit = (event) => {
     event.preventDefault()
+
+    history.push('/customer/')
+
     const headers = {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': '*',
@@ -45,38 +51,46 @@ const Login = (props) => {
   return (
     <Container>
       <Typography variant="h2">Welcome back!</Typography>
-      <Typography variant="subtitle">
-        Please log in before confirming your order.
-      </Typography>
-      <form noValidate autoComplete="off" onSubmit={handle_form_submit}>
-        <Grid container direction="column">
-          <Grid item style={{ marginTop: '1em' }}>
-            <TextField
-              required
-              name="email"
-              label="Email Address"
-              variant="outlined"
-            />
-          </Grid>
-          <Grid item style={{ marginTop: '1em' }}>
-            <TextField
-              required
-              name="password"
-              type="password"
-              label="Password"
-              variant="outlined"
-            />
-          </Grid>
-        </Grid>
-        <Button
-          variant="contained"
-          color="primary"
-          disableElevation
-          style={{ marginTop: '1em' }}
-        >
-          Sign in
-        </Button>
-      </form>
+      {auth ? (
+        `You are logged in as ${jwt.decode(auth).given_name} ${
+          jwt.decode(auth).family_name
+        }`
+      ) : (
+        <>
+          <Typography variant="subtitle">
+            Please log in before confirming your order.
+          </Typography>
+          <form noValidate autoComplete="off" onSubmit={handle_form_submit}>
+            <Grid container direction="column">
+              <Grid item style={{ marginTop: '1em' }}>
+                <TextField
+                  required
+                  name="email"
+                  label="Email Address"
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid item style={{ marginTop: '1em' }}>
+                <TextField
+                  required
+                  name="password"
+                  type="password"
+                  label="Password"
+                  variant="outlined"
+                />
+              </Grid>
+            </Grid>
+            <Button
+              variant="contained"
+              color="primary"
+              disableElevation
+              style={{ marginTop: '1em' }}
+            >
+              Sign in
+            </Button>
+          </form>
+        </>
+      )}
     </Container>
   )
 }
