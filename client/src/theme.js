094d6d083f5a0bab19781b874e94698271583d21
yeekayl <yeekayl@student.unimelb.edu.au>
2021-05-25 13:02:44 +1000

    added contrast text colour to theme

--- client/src/theme.js
+++ client/src/theme.js
@@ -19,9 +19,11 @@ const theme = createMuiTheme({
     },
     green: {
       main: '#77DDAB',
+      contrastText: '#ffffff'
     },
     brown: {
       main: '#776533',
+      contrastText: '#ffffff'
     },
     grey: {
       main: '#959595',
