634c0e4d1dafd1469894b35cd79e0454ca3e23a9
Wen Yee Ang <wenyee-ang@users.noreply.github.com>
2021-05-25 17:39:21 +1000

    Implemented Customer Registration.

--- client/src/CustomerApp.js
+++ client/src/CustomerApp.js
@@ -6,9 +6,11 @@ import Nav3 from './customer/Nav3'
 import { Switch, Route } from 'react-router-dom'
 import SplashPage from './customer/Pages/SplashPage'
 import Login from './customer/Pages/Login'
+import Registration from './customer/Pages/Registration'
 import MyOrder from './customer/Pages/MyOrder'
 import VanDetails from './customer/Pages/VanDetails'
 import Orders from './customer/Pages/Orders'
+
 
 const CustomerApp = () => {
   const [location, setLocation] = useState(null)
@@ -44,6 +46,11 @@ const CustomerApp = () => {
           exact
           path="/customer/login"
           render={() => <Login auth={auth} setAuth={setAuth} />}
+        />
+        <Route
+          exact
+          path="/customer/registration"
+          render={() => <Registration auth={auth} setAuth={setAuth} />}
         />
         <Route
           exact
