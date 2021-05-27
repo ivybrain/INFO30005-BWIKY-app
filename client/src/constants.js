79fcaa8fa74614ee44d4f906401d30a1ad920964
Ivy Brain <ibrain@student.unimelb.edu.au>
2021-05-27 19:28:15 +1000

    Changes to use production backend

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,3 +1,3 @@
-//export const API_URL = 'https://bwiky-api.herokuapp.com'
+export const API_URL = 'https://bwiky-api.herokuapp.com'
 
-export const API_URL = 'http://localhost:8080'
+//export const API_URL = 'http://localhost:8080'
