0725c5f3f03c90c6bdaae2bed4760738c6a48295
Ben Cook <mcbeanoo@gmail.com>
2021-05-29 03:14:28 +1000

    test not working

---
+++ server/__tests__/unit/unitTest.js
@@ -0,0 +1,40 @@
+// const mongoose = require('mongoose')
+// const vendorsController = require('../../controllers/vendorsC')
+// const Vendor = require('../../models/Vendor')
+
+describe('Unit testing vendor_update from vendorsC.js', () => {
+  test('test test', () => {
+    expect(123).toEqual(123)
+  })
+  // const req = {
+  //   params: { vendor_id: '607710190959c969a0325846' },
+  // }
+
+  // const res = {
+  //   render: jest.fn(),
+  // }
+
+  // beforeAll(() => {
+  //   res.render.mockClear()
+
+  //   Vendor.findById(req.vendor) = jest
+  //     .fn()
+  //     .mockResolvedValue([
+  //       { _id: '607710190959c969a0325846', van_name: 'Van Damme' },
+  //     ])
+
+  //   Vendor.findById.mockImplementationOnce(() => ({
+  //     lean: jest.fn().mockReturnValue({
+  //       _id: '607710190959c969a0325846',
+  //       van_name: 'Van Damme',
+  //     }),
+  //   }))
+
+  //   vendorsController.vendor_update(req, res)
+  // })
+
+  // test("Test Case 1: Testing updating the ready status of van Van Damme, id = 607710190959c969a0325846", () => {
+  //   expect(res.render).toHaveBennCalledTimes(1)
+  //   expect(res.render).toHaveBeenCalledWith('')
+  // })
+})
